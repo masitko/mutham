d8d7bc12ae28dd1beacc153074d0b0b3f914d0df
masitko <masitko@gmail.com>
2019-01-25 16:04:47 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Muntham House Project
---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+use LaravelEnso\Companies\app\Models\Company;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'mandatary_id' => null,
+        'name' => $faker->company,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'fax' => $faker->phoneNumber,
+        'bank' => $faker->company,
+        'bank_account' => $faker->bankAccountNumber,
+        'obs' => $faker->sentence,
+        'pays_vat' => $faker->boolean,
+    ];
+});
